b9a7b680bc904bf239fca603a395e52593a94508
manojnarayan <42382312+manojnarayan@users.noreply.github.com>
2024-03-19 17:49:15 +0530

    Update user-input-to-file.sh

--- shellscript/Normal-condition/user-input-to-file.sh
+++ shellscript/Normal-condition/user-input-to-file.sh
@@ -2,7 +2,7 @@
 echo "Enter the file name"
 read fil
 touch $fil
-echo "File $fil is created anf below are the details of the file $fil"
+echo "File $fil is created and below are the details of the file $fil"
 ls -ltr $fil
 echo "Enter the first input to be added to  the file $fil"
 read nu1
@@ -16,17 +16,17 @@ cat $fil
 
 Here's a breakdown of each part of the script:
 
-Prompting for File Name: The script prompts the user to enter a file name using the echo and read commands.
+Prompting for File Name:- The script prompts the user to enter a file name using the echo and read commands.
 
-Creating the File: The touch command is used to create the file with the name entered by the user.
+Creating the File:- The touch command is used to create the file with the name entered by the user.
 
-Displaying File Details: The ls -ltr command is used to display detailed information about the file, such as its permissions, owner, size, and modification time.
+Displaying File Details:- The ls -ltr command is used to display detailed information about the file, such as its permissions, owner, size, and modification time.
 
-Prompting for Inputs: The script prompts the user to enter two inputs using the read command.
+Prompting for Inputs:- The script prompts the user to enter two inputs using the read command.
 
-Appending Inputs to File: The >> operator is used to append the inputs entered by the user to the file.
+Appending Inputs to File:- The >> operator is used to append the inputs entered by the user to the file.
 
-Displaying File Contents: The cat command is used to display the contents of the file.
+Displaying File Contents:- The cat command is used to display the contents of the file.
 
 Overall, this script allows users to create a file, add content to it, and view its details and contents.
 
